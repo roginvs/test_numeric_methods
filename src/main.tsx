4627fcd1c0075bfe7f26b7cda7654efceac873e4
Vasilii Rogin <vasilii@giosg.com>
2019-12-08 14:18:04 +0200

    Add routing

--- src/main.tsx
+++ src/main.tsx
@@ -14,19 +14,52 @@ import {
   NavbarText,
 } from "reactstrap";
 import { Hord } from "./hord";
+import { Gauss } from "./gauss";
+
+const ROUTINGS = ["span", "gauss"] as const;
+type Routing = typeof ROUTINGS[number];
+const routesToText = (r: Routing) => {
+  return {
+    span: "Хорды",
+    gauss: "Гаусс",
+  }[r];
+};
 
 export const Root = () => {
-  // const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-  // const toggle = () => setIsOpen(!isOpen);
+  const toggle = () => setIsOpen(!isOpen);
 
+  const forceUpdate = useState<{}>()[1];
+
+  const hash = document.location.hash.replace("#", "") as Routing | "";
   return (
     <div>
       <Navbar color="light" light expand="md">
         <NavbarBrand href="/">Demo</NavbarBrand>
+
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            {ROUTINGS.map(r => (
+              <NavItem key={r}>
+                <NavLink
+                  active={hash === r}
+                  href={`#${r}`}
+                  onClick={e => {
+                    e.preventDefault();
+                    document.location.hash = `#${r}`;
+                    forceUpdate({});
+                  }}
+                >
+                  {routesToText(r)}
+                </NavLink>
+              </NavItem>
+            ))}
+          </Nav>
+        </Collapse>
       </Navbar>
       <div className="container-sm">
-        <Hord />
+        {hash === "span" ? <Hord /> : hash === "gauss" ? <Gauss /> : <div>Демо</div>}
       </div>
     </div>
   );
