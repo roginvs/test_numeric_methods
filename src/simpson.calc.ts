ab4fe32f727195e694a0f2fb94c4a5aa4f8b1cd8
Vasilii Rogin <vasilii@giosg.com>
2020-01-01 18:11:32 +0200

    WiP

--- src/simpson.calc.ts
+++ src/simpson.calc.ts
@@ -1,3 +1,20 @@
 export function calculate(a: number, b: number, c: number, epsilon: number) {
-  return "todo";
+  let msg = "";
+  const addMsg = (s: string) => {
+    console.info(s);
+    msg += `${s}\n`;
+  };
+
+  for (const l of [
+    {
+      name: "e^(ax) * sin (bx)",
+      f: (x: number) => Math.exp(a * x) * Math.sin(b * x),
+      // D[Exp[a * x] * Sin[b * x], {x, 4}]
+      max4d: () => 2,
+    },
+  ]) {
+    addMsg(`Считаем функцию ${l.name}`);
+  }
+
+  return msg;
 }
