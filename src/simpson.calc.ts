959c2e3708959cf496f7cacf63f70b4507b9fead
Vasilii Rogin <vasilii@giosg.com>
2020-02-01 12:48:51 +0200

    Simplify Simpson

--- src/simpson.calc.ts
+++ src/simpson.calc.ts
@@ -1,19 +1,52 @@
-export function calculate(a: number, b: number, c: number, epsilon: number) {
+interface Func {
+  a: number;
+  b: number;
+  f: (x: number) => number;
+  name: string;
+  max4d: number;
+}
+
+const FUNCS: Func[] = [
+  {
+    name: "x*sqrt(x+1)",
+    f: (x: number) => x * Math.sqrt(x + 1),
+    max4d: 1.5,
+    a: 0,
+    b: 1,
+  },
+];
+
+export function calculate(epsilon: number) {
   let msg = "";
   const addMsg = (s: string) => {
     console.info(s);
     msg += `${s}\n`;
   };
 
-  for (const l of [
-    {
-      name: "e^(ax) * sin (bx)",
-      f: (x: number) => Math.exp(a * x) * Math.sin(b * x),
-      // D[Exp[a * x] * Sin[b * x], {x, 4}]
-      max4d: () => 2,
-    },
-  ]) {
-    addMsg(`Считаем функцию ${l.name}`);
+  for (const l of FUNCS) {
+    const { f, a, b } = l;
+
+    addMsg(`Считаем интеграл функции ${l.name} на [${a}, ${b}]`);
+    const M = l.max4d;
+
+    addMsg(`M = ${M}`);
+
+    const nFractial = Math.pow((M * Math.pow(b - a, 5)) / (180 * epsilon), 1 / 4);
+    const nCeil = Math.ceil(nFractial);
+    const n = nCeil % 2 === 0 ? nCeil : nCeil + 1;
+    addMsg(`n = ${n}`);
+
+    const h = (b - a) / n;
+    addMsg(`h = ${h}`);
+
+    let s = (f(a) - f(b)) / 2;
+    for (let i = 1; i <= n / 2; i++) {
+      const x = a + 2 * i * h;
+      s = s + f(x) + 2 * f(x - h);
+    }
+    s = (s * 2 * h) / 3;
+    addMsg(`Интеграл = ${s}`);
+    addMsg("");
   }
 
   return msg;
--- src/simpson.tsx
+++ src/simpson.tsx
@@ -3,10 +3,10 @@ import React, { useState } from "react";
 import { calculate } from "./simpson.calc";
 
 export const Simpson = () => {
-  const [a, setA] = useState(0.73);
-  const [b, setB] = useState(1.46);
-  const [c, setC] = useState(0.72);
-  const [e, setE] = useState(0.00001);
+  //  const [a, setA] = useState(0.73);
+  //  const [b, setB] = useState(1.46);
+  //  const [c, setC] = useState(0.72);
+  const [e, setE] = useState(0.0000001);
 
   const [msg, setMsg] = useState<string | undefined>();
 
@@ -16,38 +16,6 @@ export const Simpson = () => {
       {msg === undefined ? (
         <>
           <div className="mb-2">
-            <div className="form-group ">
-              <label className="">
-                Параметры для функции e
-                <sup>
-                  <i>ax</i>
-                </sup>
-                sin<i>bx</i>{" "}
-              </label>
-              <input
-                className="form-control"
-                type="number"
-                value={a}
-                onChange={e => setB(parseInt(e.target.value))}
-              />
-              <input
-                className="form-control"
-                type="number"
-                value={b}
-                onChange={e => setB(parseInt(e.target.value))}
-              />
-            </div>
-
-            <div className="form-group ">
-              <label className="">Параметры для функции 1/sqrt(x*x + c)</label>
-              <input
-                className="form-control"
-                type="number"
-                value={c}
-                onChange={e => setC(parseInt(e.target.value))}
-              />
-            </div>
-
             <div className="form-group ">
               <label className="">Точность</label>
               <input
@@ -64,7 +32,7 @@ export const Simpson = () => {
               onClick={ev => {
                 ev.preventDefault();
                 try {
-                  setMsg(calculate(a, b, c, e));
+                  setMsg(calculate(e));
                 } catch (e) {
                   setMsg(`${e.name} ${e.message}`);
                 }
